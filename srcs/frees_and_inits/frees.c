24e048370c43adaad826c07fb44bcd954b466257
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r10s3.42lisboa.com>
2023-01-14 22:00:33 +0000

    Alpha-ordering only if args for export are non-existent

--- srcs/frees_and_inits/frees.c
+++ srcs/frees_and_inits/frees.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   frees.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aappleto <aappleto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 18:59:00 by aappleto          #+#    #+#             */
-/*   Updated: 2023/01/14 18:25:27 by aappleto         ###   ########.fr       */
+/*   Updated: 2023/01/16 16:45:30 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,35 +35,3 @@ void	free_promptinfo(t_promptinfo *prompt_info)
 	free(prompt_info->user);
 	dict_free(prompt_info->shenv);
 }
-
-//void	dict_freeone(t_dict *dict)
-//{
-//	free(dict->key);
-//	free(dict->value);
-//	free(dict);
-//}
-
-//void	dict_free(t_dict *dict)
-//{
-//	t_dict	*after;
-
-//	//TODO: Proctections
-//	while (dict != NULL)
-//	{
-//		after = dict->next;
-//		dict_freeone(dict);
-//		dict = after;
-//	}
-//}
-
-//void	dict_shallowfree(t_dict *dict)
-//{
-//	t_dict	*after;
-
-//	while (dict != NULL)
-//	{
-//		after = dict->next;
-//		free(dict);
-//		dict = after;
-//	}
-//}
