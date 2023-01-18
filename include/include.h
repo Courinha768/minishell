4e7422ae2030b8cc146894e9d28c430924f299d2
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c2r3s6.42lisboa.com>
2023-01-18 17:45:54 +0000

    Signals completed

--- include/include.h
+++ include/include.h
@@ -6,7 +6,7 @@
 /*   By: aappleto <aappleto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 13:05:43 by aappleto          #+#    #+#             */
-/*   Updated: 2023/01/18 17:19:32 by aappleto         ###   ########.fr       */
+/*   Updated: 2023/01/18 17:44:21 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,5 @@
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <sys/wait.h>
-
 
 #endif
--- srcs/utils/shell_commands.c
+++ srcs/utils/shell_commands.c
@@ -6,7 +6,7 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 20:55:20 by aappleto          #+#    #+#             */
-/*   Updated: 2023/01/17 23:25:25 by amaria-d         ###   ########.fr       */
+/*   Updated: 2023/01/18 17:45:46 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,37 +20,19 @@ void	clear_shell(void)
 void	sig_print(int signal)
 {
 	(void)signal;
+	rl_replace_line("", 0);
+	printf("\n");
+	rl_on_new_line();
+	rl_redisplay();
 }
 
 void sig_hnd(int sig){ (void)sig; printf("(SIG)"); }
 
-
+/**
+ * Process received signals
+*/
 void	shell_signal(void)
 {
-	// struct sigaction act;
-	// sigset_t		ignset;
-
-	// SIGINT and SIGQUIT working!
-	//Alert: ^C will show up and that's fine
 	signal(SIGINT, sig_print);
 	signal(SIGQUIT, SIG_IGN);
-	
-	// sigfillset(&act.sa_mask);
-	// sigemptyset(&act.sa_mask);
-	// sigaddset(&act.sa_mask, SIGINT);
-	// sigaddset(&act.sa_mask, SIGQUIT);
-
-	// act.sa_handler = SIG_IGN;
-	// act.sa_flags = 0;
-	
-	// sigaction(SIGINT, &act, NULL);
-	// sigaction(SIGQUIT, &act, NULL);
-	
-
-
-
-	// signal(SIGINT, sig_hnd);
-	// termios.c_cc[VEOF] = 0; // ^C
-	// termios.c_cc[VINTR] = 0; // ^D
-// 	signal(SIGTSTP, SIG_IGN);
 }
